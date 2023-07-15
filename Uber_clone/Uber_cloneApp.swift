85743f084993c4a162638de6ab0ca8493927e53d
Mahendra raut <mahendra.raut@payiq.net>
2023-07-15 13:00:24 +0530

    Initial Commit

---
+++ Uber_clone/Uber_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_cloneApp.swift
+//  Uber_clone
+//
+//  Created by Nayan Khadase on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Uber_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
