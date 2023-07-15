ba00f5778a199e88605b5aed0a2b669957b6532f
Mahendra raut <mahendra.raut@payiq.net>
2023-07-16 00:56:10 +0530

    Added route from userlocation to destination

--- Uber_clone/Core/Home/Views/MapViewRepresentable.swift
+++ Uber_clone/Core/Home/Views/MapViewRepresentable.swift
@@ -33,6 +33,7 @@ struct MapViewRepresentable: UIViewRepresentable{
             
             // add annotation
             context.coordinator.addAndSelectAbbotation(withCoordinate: selectedLocation)
+            context.coordinator.configurePloyline(withdestination: selectedLocation)
         }
     }
     
@@ -40,9 +41,11 @@ struct MapViewRepresentable: UIViewRepresentable{
         return Coordinator(self)
     }
     
-    //MARK: - Coordinator
+    //MARK: - Coordinator, MKMapViewDelegate
+    
     class Coordinator: NSObject, MKMapViewDelegate{
         let parent: MapViewRepresentable
+        var userLocation: CLLocationCoordinate2D?
         
         init(_ parent: MapViewRepresentable) {
             self.parent = parent
@@ -51,9 +54,19 @@ struct MapViewRepresentable: UIViewRepresentable{
         
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let coordinate = userLocation.coordinate
+            self.userLocation = coordinate
+            
             // create region center around specific coordinate
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
                 self.parent.mapView.setRegion(region, animated: true) // update region for map view
+        }
+        
+        //(delegate method which tels map to draw overlay) use to draw a overlay on the map
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let render = MKPolylineRenderer(overlay: overlay)
+            render.strokeColor = .systemBlue
+            render.lineWidth = 6
+            return render
         }
         
         //MARK: - helper
@@ -74,6 +87,33 @@ struct MapViewRepresentable: UIViewRepresentable{
             parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
         }
         
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping (MKRoute) -> Void){
+            let userplacemark = MKPlacemark(coordinate: userLocation)
+            let destiPlacemark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userplacemark)
+            request.destination = MKMapItem(placemark: destiPlacemark)
+            
+            let direction = MKDirections(request: request)
+            direction.calculate { response, error in
+                if let error = error{
+                    print(error.localizedDescription)
+                    return
+                }
+                
+                guard let route = response?.routes.first else{return}
+                
+                completion(route)
+            }
+        }
+        
+        func configurePloyline(withdestination destination: CLLocationCoordinate2D){
+            guard let userLocation = userLocation else{return}
+            self.getDestinationRoute(from: userLocation, to: destination) { route in
+                // add polyline layer
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
         
         
         
